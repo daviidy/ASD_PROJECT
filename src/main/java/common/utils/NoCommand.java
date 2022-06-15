7d4fbede722442aa2b57b991057cf7b72db149a5
adugnata <a.tesfamichael.a@gmail.com>
2022-06-15 17:21:00 -0500

    renamed the subject interface

--- src/main/java/common/utils/NoCommand.java
+++
@@ -1,9 +0,0 @@
-package common.utils;
-
-import framework.command.Command;
-import framework.ui.IUIInvoker;
-
-public class NoCommand implements Command {
-    @Override
-    public void execute(IUIInvoker iuiInvoker) {}
-}
